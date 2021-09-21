f77a5fc671d65b5f534a19526ce296ab80eaa69a
kkyou <holmesqiao@gmail.com>
2021-09-21 15:31:20 +0800

    Add normal to Class hit

--- hit.h
+++ hit.h
@@ -17,21 +17,29 @@ public:
   Hit() { material = NULL; }
   Hit(float _t, Material *m) { 
     t = _t; material = m; }
+  Hit(float _t, Material *m, Vec3f n): t(_t), material(m), normal(n) {} //add from Assignment2
   Hit(const Hit &h) { 
     t = h.t; 
     material = h.material; 
-    intersectionPoint = h.intersectionPoint; }
+    intersectionPoint = h.intersectionPoint; 
+    normal = h.normal;
+  }
   ~Hit() {}
 
   // ACCESSORS
   float getT() const { return t; }
   Material* getMaterial() const { return material; }
   Vec3f getIntersectionPoint() const { return intersectionPoint; }
+  Vec3f getNormal() const { return normal; } //add from Assignment2
   
   // MODIFIER
   void set(float _t, Material *m, const Ray &ray) {
     t = _t; material = m; 
     intersectionPoint = ray.pointAtParameter(t); }
+  void set(float _t, Material *m, Vec3f n, const Ray &ray){ 
+    t = _t; material = m; normal = n;
+    intersectionPoint = ray.pointAtParameter(t);
+  }
 
 private: 
 
@@ -39,6 +47,7 @@ private:
   float t;
   Material *material;
   Vec3f intersectionPoint;
+  Vec3f normal; //add from Assignment2
 
 };
 
