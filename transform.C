00d4c9818fe7669ecf41c6ea04c442544cc44a99
kkyou <holmesqiao@gmail.com>
2021-10-08 19:42:54 +0800

    normalize direction & normal & some bugs

--- transform.C
+++ transform.C
@@ -11,15 +11,18 @@ Transform::Transform(Matrix &m, Object3D *o){
 bool Transform::intersect(const Ray &r, Hit &h, float tmin){
     //transform ray from word space to object space
     Vec3f dir_transform = r.getDirection();
-    Vec3f ori_transform = r.getOrigin();
+    Vec4f ori_transform(r.getOrigin(), 1.0);
     m_inverse.Transform(dir_transform);
     m_inverse.Transform(ori_transform);
-    //dir_transform.Normalize();
-    Ray _r(ori_transform, dir_transform);
+    ori_transform.DivideByW();
+    float dir_size = dir_transform.Length();
+    dir_transform.Normalize();
+    Ray _r(ori_transform.xyz(), dir_transform);
     if (object->intersect(_r, h, tmin)){
         Vec3f word_normal = h.getNormal();
         m_inverse_transpose.Transform(word_normal);
-        h.set(h.getT(), h.getMaterial(), word_normal, r);
+        word_normal.Normalize();
+        h.set(h.getT() / dir_size, h.getMaterial(), word_normal, r);
         return true;
     }
     return false;
