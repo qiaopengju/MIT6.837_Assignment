508eacefdc1ba92ab2100651c897b975a0131b67
kkyou <holmesqiao@gmail.com>
2021-10-09 10:37:05 +0800

    update intersect function to transform ray

--- obj/plane.C
+++ obj/plane.C
@@ -6,12 +6,17 @@ Plane::Plane(Vec3f &_noraml, float _d, Material *_m) : normal(_noraml), d(_d){
 }
 
 bool Plane::intersect(const Ray &r, Hit &h, float tmin){
-    if (r.getDirection().Dot3(normal) == 0) return false; //光线方向与平面平行
-    float t = (d - r.getOrigin().Dot3(normal)) / (r.getDirection().Dot3(normal));
+    //标准化方向向量
+    Vec3f dir_nor = r.getDirection();
+    float dir_len = dir_nor.Length();
+    dir_nor.Normalize();
+
+    if (dir_nor.Dot3(normal) == 0) return false; //光线方向与平面平行
+    float t = (d - r.getOrigin().Dot3(normal)) / (dir_nor.Dot3(normal));
     if (t < 0) return false; //在光线背面
     //与射线相交
-    if (t < tmin) return false;
-    if (h.getMaterial() != NULL && h.getT() < t) return true;
-    h.set(t, material, normal, r);
+    if (t/dir_len < tmin) return false;
+    if (h.getMaterial() != NULL && h.getT() < t/dir_len) return true;
+    h.set(t/dir_len, material, normal, r);
     return true;
 }
